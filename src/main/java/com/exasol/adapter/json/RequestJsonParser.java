63f0ef9245d487b11345c90cfed861b4e7ad69f9
Anastasiia Sergienko <anastasiia.sergienko@exasol.com>
2019-01-30 11:30:52 +0100

    #9 changed .travis.yml file to make travis CI work

--- src/main/java/com/exasol/adapter/json/RequestJsonParser.java
+++ src/main/java/com/exasol/adapter/json/RequestJsonParser.java
@@ -1,6 +1,5 @@
 package com.exasol.adapter.json;
 
-import com.exasol.adapter.AdapterException;
 import com.exasol.adapter.metadata.*;
 import com.exasol.adapter.metadata.DataType.ExaCharset;
 import com.exasol.adapter.metadata.DataType.IntervalType;
