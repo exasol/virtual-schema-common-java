6e492afb75b33d2b57dceea260450c8d5b380785
Sebastian Bär <sebastian.baer@exasol.com>
2019-03-26 15:17:08 +0100

    #45: Fixed log level JSON key.

--- src/main/java/com/exasol/adapter/metadata/SchemaMetadataPropertyConstants.java
+++ src/main/java/com/exasol/adapter/metadata/SchemaMetadataPropertyConstants.java
@@ -2,7 +2,7 @@ package com.exasol.adapter.metadata;
 
 public final class SchemaMetadataPropertyConstants {
     public static final String REMOTE_DEBUG_CONFIG_KEY = "DEBUG_ADDRESS";
-    public static final String LOG_LEVEL_KEY = "DEBUG_LEVEL";
+    public static final String LOG_LEVEL_KEY = "LOG_LEVEL";
 
     private SchemaMetadataPropertyConstants() {
         // prevent instantiation
