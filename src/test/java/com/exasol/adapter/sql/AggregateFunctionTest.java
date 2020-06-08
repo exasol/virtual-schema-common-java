d2e2f7ae2fd9e863f1111642fba87b1faaf0b0dd
anastasiiasergienko <anastasiia.sergienko@exasol.com>
2020-06-08 10:06:22 +0200

    #139: improved test coverage

--- src/test/java/com/exasol/adapter/sql/AggregateFunctionTest.java
+++ src/test/java/com/exasol/adapter/sql/AggregateFunctionTest.java
@@ -1,9 +1,11 @@
 package com.exasol.adapter.sql;
 
-import org.junit.jupiter.api.Test;
-
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 class AggregateFunctionTest {
     @Test
@@ -14,5 +16,15 @@ class AggregateFunctionTest {
     @Test
     void testIsSimpleTrue() {
         assertTrue(AggregateFunction.COUNT.isSimple());
+    }
+
+    @Test
+    void testToStringWithSpecialName() {
+        assertThat(AggregateFunction.GEO_INTERSECTION_AGGREGATE.toString(), equalTo("ST_INTERSECTION"));
+    }
+
+    @Test
+    void testToString() {
+        assertThat(AggregateFunction.AVG.toString(), equalTo("AVG"));
     }
 }
