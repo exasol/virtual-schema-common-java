554a4d206c9fd609f539d7fc34afea0b2ce8e860
Anastasiia Sergienko <46891819+AnastasiiaSergienko@users.noreply.github.com>
2020-12-22 08:34:40 +0100

    Feature/188 generate explicit select list (#194)
    *  #188: Replaced SELECT * with an explicit select list
    Co-authored-by: Sebastian Bär <sebastian.baer@exasol.com>

---
+++ doc/changes/changes_14.0.0.md
@@ -0,0 +1,11 @@
+# Virtual Schema Common Java 14.0.0, released 2020-XX-XX
+
+Code name: 
+
+## Feature
+
+* #188: Create a select columns list from the involved tables list when the select list is missing.
+
+## Documentation
+
+* #190: Improved `SELECTLIST_EXPRESSIONS` documentation
--- src/main/java/com/exasol/adapter/sql/SqlSelectList.java
+++ src/main/java/com/exasol/adapter/sql/SqlSelectList.java
@@ -5,54 +5,39 @@ import java.util.List;
 import com.exasol.adapter.AdapterException;
 
 public final class SqlSelectList extends SqlExpressionList {
-    private final SqlSelectListType type;
-
-    private SqlSelectList(final SqlSelectListType type, final List<SqlNode> selectList) {
+    private SqlSelectList(final List<SqlNode> selectList) {
         super(selectList);
-        this.type = type;
     }
 
     /**
-     * Creates a SqlSelectList for SELECT *. See {@link SqlSelectListType#SELECT_STAR}.
+     * Creates a SqlSelectList that uses an arbitrary value.
      *
-     * @return the new SqlSelectList.
-     */
-    public static SqlSelectList createSelectStarSelectList() {
-        return new SqlSelectList(SqlSelectListType.SELECT_STAR, null);
-    }
-
-    /**
-     * Creates a SqlSelectList that uses an arbitrary value. See {@link SqlSelectListType#ANY_VALUE}.
-     *
-     * @return the new SqlSelectList.
+     * @return new SqlSelectList.
      */
     public static SqlSelectList createAnyValueSelectList() {
-        return new SqlSelectList(SqlSelectListType.ANY_VALUE, null);
+        return new SqlSelectList(null);
     }
 
     /**
-     * Creates a regular SqlSelectList. See {@link SqlSelectListType#REGULAR}.
+     * Creates a regular SqlSelectList.
      *
-     * @param selectList The selectList needs at least one element.
-     * @return the new SqlSelectList.
+     * @param selectList selectList needs at least one element.
+     * @return new SqlSelectList.
      */
     public static SqlSelectList createRegularSelectList(final List<SqlNode> selectList) {
         if ((selectList == null) || selectList.isEmpty()) {
-            throw new IllegalArgumentException(
-                    "SqlFunctionAggregateGroupConcat constructor expects an argument." + "But the list is empty.");
+            throw new IllegalStateException("Cannot create a select list, because the list of arguments is empty.");
         }
-        return new SqlSelectList(SqlSelectListType.REGULAR, selectList);
-    }
-
-    public boolean isRequestAnyColumn() {
-        return this.type == SqlSelectListType.ANY_VALUE;
+        return new SqlSelectList(selectList);
     }
 
     /**
-     * @return {@code true} if this is "{@code SELECT *}"
+     * Check if this SELECT has an explicit columns list.
+     * 
+     * @return true if has an explicit columns list
      */
-    public boolean isSelectStar() {
-        return this.type == SqlSelectListType.SELECT_STAR;
+    public boolean hasExplicitColumnsList() {
+        return super.getExpressions() != null && !super.getExpressions().isEmpty();
     }
 
     @Override
