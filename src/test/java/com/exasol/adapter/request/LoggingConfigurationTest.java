464632f34bda439c4340490ec5890ce86922913f
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-11-17 08:13:11 +0100

    Extract serialization assertion to separate class

--- src/test/java/com/exasol/adapter/request/LoggingConfigurationTest.java
+++ src/test/java/com/exasol/adapter/request/LoggingConfigurationTest.java
@@ -3,15 +3,17 @@ package com.exasol.adapter.request;
 import static com.exasol.adapter.AdapterProperties.DEBUG_ADDRESS_PROPERTY;
 import static com.exasol.adapter.AdapterProperties.LOG_LEVEL_PROPERTY;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import java.io.*;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 
 import org.junit.jupiter.api.Test;
+
+import com.exasol.adapter.SerializationTestUtil;
 
 class LoggingConfigurationTest {
     final Map<String, String> properties = new HashMap<>();
@@ -69,28 +71,6 @@ class LoggingConfigurationTest {
     @Test
     void testSerializable() throws ClassNotFoundException, IOException {
         final LoggingConfiguration config = createLoggingConfiguration(this.properties);
-        assertSerializable(config);
-    }
-
-    static <T> void assertSerializable(final T object) throws IOException, ClassNotFoundException {
-        final byte[] serialized = serialize(object);
-        final Object deserialized = deserialize(serialized, object.getClass());
-        assertThat(deserialized, not(nullValue()));
-    }
-
-    static <T> byte[] serialize(final T object) throws IOException {
-        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        final ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-        objectOutputStream.writeObject(object);
-        objectOutputStream.close();
-        return outputStream.toByteArray();
-    }
-
-    static <T> T deserialize(final byte[] serializedObject, final Class<T> clazz)
-            throws IOException, ClassNotFoundException {
-        final ByteArrayInputStream inputStream = new ByteArrayInputStream(serializedObject);
-        final ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-        final Object result = objectInputStream.readObject();
-        return clazz.cast(result);
+        SerializationTestUtil.assertSerializable(config);
     }
 }
