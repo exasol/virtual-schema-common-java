c3a32179a36b7278270d47bb24389359447a52af
Maxim Lapan <maxim.lapan@exasol.com>
2025-11-18 21:09:26 +0100

    A bit more tests

--- src/test/java/com/exasol/adapter/sql/SqlFunctionAggregateListaggTest.java
+++ src/test/java/com/exasol/adapter/sql/SqlFunctionAggregateListaggTest.java
@@ -2,12 +2,15 @@ package com.exasol.adapter.sql;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
 
 class SqlFunctionAggregateListaggTest {
     @Test
@@ -21,5 +24,13 @@ class SqlFunctionAggregateListaggTest {
     @ValueSource(strings = { "WITH COUNT", "WITHOUT COUNT" })
     void testTruncationTypeParsing(final String value) {
         assertDoesNotThrow(() -> SqlFunctionAggregateListagg.Behavior.TruncationType.parseTruncationType(value));
+    }
+
+    @Test
+    void testGetChildren() {
+        final SqlNode argument = new SqlLiteralString("test string");
+        final SqlFunctionAggregateListagg.Behavior behaviour = new SqlFunctionAggregateListagg.Behavior(SqlFunctionAggregateListagg.BehaviorType.TRUNCATE);
+        final SqlFunctionAggregateListagg listagg = SqlFunctionAggregateListagg.builder(argument, behaviour).build();
+        assertThat(listagg.getChildren(), equalTo(List.of(argument)));
     }
 }
